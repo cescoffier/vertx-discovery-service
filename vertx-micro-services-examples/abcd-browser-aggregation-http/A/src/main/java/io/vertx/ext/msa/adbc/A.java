720ec9b9bc03a4b8e6e84d91d7e92f8116eb6c1e
Clement Escoffier <clement.escoffier@gmail.com>
2016-03-15 10:08:25 +0100

    Work on the ABCD example - browser aggregation HTTP

---
+++ vertx-micro-services-examples/abcd-browser-aggregation-http/A/src/main/java/io/vertx/ext/msa/adbc/A.java
@@ -0,0 +1,44 @@
+package io.vertx.ext.msa.adbc;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+/**
+ * A publishes a static HTML page that would consume the A, B, C and D Rest services.
+ * A also publishes the A REST services and a REST call to retrieve B, C and D addressed.
+ * <p>
+ * No circuit-breaker involved, as it should be managed in the browser.
+ * No discovery involved, as it should be managed in the browser.
+ */
+public class A extends AbstractVerticle {
+
+  @Override
+  public void start() throws Exception {
+    Router router = Router.router(vertx);
+
+    // Publish assets
+    router.route().handler(StaticHandler.create());
+
+    // Enable Cors
+    router.route().handler(CorsHandler.create("*")
+        .allowedMethod(HttpMethod.GET)
+        .allowedHeader("Content-Type"));
+
+    // Publish the A service
+    router.route(HttpMethod.GET, "/A").handler(context -> {
+      context.response().end("Hello " + context.request().getParam("name"));
+    });
+
+    router.route(HttpMethod.GET, "/endpoints").handler(context -> {
+      String b = "http://" + System.getenv("B_APP_PORT").replace("tcp", "http");
+      //TODO c and d.
+      JsonObject result = new JsonObject()
+          .put("B", b);
+      context.response().putHeader("content-type", "application/json").end(result.encode());
+    });
+  }
+}
---
+++ vertx-micro-services-examples/abcd-browser-aggregation-http/B/src/main/java/io/vertx/ext/msa/abcd/B.java
@@ -0,0 +1,34 @@
+package io.vertx.ext.msa.abcd;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+/**
+ * B publishes the B REST services.
+ * <p>
+ * No circuit-breaker involved, as it should be managed in the browser.
+ * No discovery involved, as it should be managed in the browser.
+ */
+public class B extends AbstractVerticle {
+
+  @Override
+  public void start() throws Exception {
+    Router router = Router.router(vertx);
+
+    // Publish assets
+    router.route().handler(StaticHandler.create());
+
+    // Enable Cors
+    router.route().handler(CorsHandler.create("*")
+        .allowedMethod(HttpMethod.GET)
+        .allowedHeader("Content-Type"));
+
+    // Publish the B service
+    router.route(HttpMethod.GET, "/").handler(context -> {
+      context.response().end("Hola " + context.request().getParam("name"));
+    });
+  }
+}
